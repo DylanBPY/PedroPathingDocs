5dc59459d265dc4429036e19de0bf53bcd3ab526
nesetkab <foulofdoom@gmail.com>
2026-03-19 22:36:09 -0600

    fixing bum issue alskjdflkasjdlkfaj :3

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,6 @@ export default function Layout({children}: { children: ReactNode }) {
         <html lang="en" className={instrumentSans.className} suppressHydrationWarning>
         <body className="flex flex-col min-h-screen">
             <Provider>{children}</Provider>
-            <footer className="mt-auto border-t border-fd-border py-6 px-6 text-center text-sm text-fd-muted-foreground">
-                &copy; 2026 Pedro Pathing. Pedro Pathing is licensed under the BSD 3-Clause License. The Pedro Pathing name and logo are trademarks of Pedro Pathing.
-            </footer>
         </body>
         </html>
     );
